2a9507eb603589eb96a77ac9f6837e4dbd6d5df1
Naijil <110610851+naijilnj@users.noreply.github.com>
2024-04-10 20:54:43 +0530

    Update admin.py

--- admin/admin.py
+++ admin/admin.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
 import shared
+import requests
+
+def get_external_ip():
+    response = requests.get("https://api64.ipify.org?format=json")
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("ip")
+    else:
+        return "Unknown"
 
 # Admin credentials
 ADMIN_USERNAME = "admin"
